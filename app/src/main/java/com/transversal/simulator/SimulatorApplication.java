b6de96ff97f93cc54c8b84aa0cb16407501a0192
Gouyq <55273887+Gouyq@users.noreply.github.com>
2021-01-12 01:36:50 +0100

    Main simulator

--- app/src/main/java/com/transversal/simulator/SimulatorApplication.java
+++ app/src/main/java/com/transversal/simulator/SimulatorApplication.java
@@ -1,13 +1,32 @@
 package com.transversal.simulator;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jms.annotation.EnableJms;
+import manager.FireGeneratorThread;;
 
 @SpringBootApplication
+@EnableJms
+@ComponentScan(basePackages = { "config", "services", "jms", "manager"})
 public class SimulatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimulatorApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner SimulatorDemo() {
+		return (args) -> {
+			Thread fireGeneratorThread = new Thread(new FireGeneratorThread());
+			while (true) {
+				fireGeneratorThread.start();
+				fireGeneratorThread.join();
+				System.out.println("Suite");
+			}
+		};
 	}
 
 }
